a7c6e2df18fb780ffb1b48d2e6b0634dfccdb462
Mino777 <jomino7777@gmail.com>
2022-01-04 17:19:02 +0900

    feat: add AuditionManager Type

---
+++ CodeStarterCamp_Week5/AuditionManager.swift
@@ -0,0 +1,58 @@
+//
+//  AuditionManager.swift
+//  CodeStarterCamp_Week5
+//
+//  Created by 조민호 on 2022/01/04.
+//
+
+import Foundation
+
+class AuditionManager {
+    private(set) var totalApplicantsList: [Person]
+    private(set) var passedApplicantsList: [Person] = []
+    
+    init(totalApplicantsList: [Person]) {
+        self.totalApplicantsList = totalApplicantsList
+    }
+    
+    func cast() {
+        judge()
+        announcePassedApplicants()
+    }
+    
+    private func judge() {
+        self.totalApplicantsList.forEach { applicant in
+            guard let talent = applicant as? TalentedPerson else {
+                return
+            }
+            
+            guard talent.singing == .A else {
+                return
+            }
+            
+            guard talent.dancing == .A else {
+                return
+            }
+            
+            guard talent.acting == .A else {
+                return
+            }
+            
+            self.passedApplicantsList.append(talent)
+        }
+    }
+    
+    private func announcePassedApplicants() {
+        guard self.passedApplicantsList.isEmpty == false else {
+            print("합격자가 없습니다.")
+            return
+        }
+        
+        print("---합격자 명단---")
+        self.passedApplicantsList.forEach { applicant in
+            print(applicant.name)
+        }
+        print("--------------")
+        print("축하합니다!!")
+    }
+}
