ca348db19c03bbf381bb41d9eba808281822bbcf
Mino777 <jomino7777@gmail.com>
2022-01-04 17:14:49 +0900

    feat: add Person Type

---
+++ CodeStarterCamp_Week5/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  CodeStarterCamp_Week5
+//
+//  Created by 조민호 on 2022/01/04.
+//
+
+import Foundation
+
+class Person {
+    let name: String
+    let height: Int
+    
+    init(name: String, height: Int) {
+        self.name = name
+        self.height = height
+    }
+}
