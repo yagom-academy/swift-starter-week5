6760e2362883f9adbaf12df06f1253e69413143c
yuvinrho <yvrho@naver.com>
2022-05-10 12:36:57 +0900

    Person class 정의

--- CodeStarterCamp_Week5/main.swift
+++ CodeStarterCamp_Week5/main.swift
@@ -1,4 +1,11 @@
 import Foundation
 
-
-
+class Person {
+    let name: String
+    let height: Int
+    
+    init(name: String, height: Int) {
+        self.name = name
+        self.height = height
+    }
+}
