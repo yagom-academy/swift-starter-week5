0006b4c7f19ea0625f65c9ddaa7e04b5847f8f69
jeongmin <lj845854@gmail.com>
2022-05-10 16:55:45 +0900

    add : AuditionManager 구조체 정의

---
+++ CodeStarterCamp_Week5/AuditionManager.swift
@@ -0,0 +1,25 @@
+//
+//  AuditionManager.swift
+//  CodeStarterCamp_Week5
+//
+//  Created by 이정민 on 2022/05/10.
+//
+
+import Foundation
+
+struct AuditionManager {
+    let totalApplicantsList: [Person]
+    var passedApplicantsList: [Person] = []
+    
+    init(totalApplicantsList: [Person]) {
+        self.totalApplicantsList = totalApplicantsList
+    }
+    
+    func cast(applicant: Person) {
+        
+    }
+    
+    func announcePassedApplicants() {
+        
+    }
+}
