df15405bb2aaafd2cb897814f970238cfbb81554
yuvinrho <yvrho@naver.com>
2022-05-11 01:09:49 +0900

    TalentedPersonWithBadPersonality 클래스 정의

--- CodeStarterCamp_Week5/main.swift
+++ CodeStarterCamp_Week5/main.swift
@@ -36,5 +36,20 @@ class TalentedPerson: Person, Talent {
         self.dancing = dancing
         self.acting = acting
         super.init(name: name, height: height)
-    }    
+    }
+}
+
+class TalentedPersonWithBadPersonality: Person, Talent, BadPersonality {
+    var singing: Level
+    var dancing: Level
+    var acting: Level
+    var frequancyOfCursing: Level
+    
+    init(name: String, height: Int, singing: Level, dancing: Level, acting: Level, frequancyOfCursing: Level) {
+        self.singing = singing
+        self.dancing = dancing
+        self.acting = acting
+        self.frequancyOfCursing = frequancyOfCursing
+        super.init(name: name, height: height)
+    }
 }
