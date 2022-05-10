55460c94b31ed0396887202de91980f9ea2a6bb4
yuvinrho <yvrho@naver.com>
2022-05-11 01:07:01 +0900

    TalentedPerson 클래스 정의

--- CodeStarterCamp_Week5/main.swift
+++ CodeStarterCamp_Week5/main.swift
@@ -26,3 +26,15 @@ protocol BadPersonality {
     var frequancyOfCursing: Level { get set }
 }
 
+class TalentedPerson: Person, Talent {
+    var singing: Level
+    var dancing: Level
+    var acting: Level
+    
+    init(name: String, height: Int, singing: Level, dancing: Level, acting: Level) {
+        self.singing = singing
+        self.dancing = dancing
+        self.acting = acting
+        super.init(name: name, height: height)
+    }    
+}
