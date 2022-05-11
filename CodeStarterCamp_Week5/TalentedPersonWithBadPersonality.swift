2364780b97911ecc013dc7d27430bcda563d596a
wolframhwang <hzw94@naver.com>
2022-05-11 23:27:13 +0900

    [feat] Mission5: Step1: TalentedPersonWithBadPersonality 구현

--- CodeStarterCamp_Week5/TalentedPersonWithBadPersonality.swift
+++ CodeStarterCamp_Week5/TalentedPersonWithBadPersonality.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class TalentedPersonWithBadPersonality: Person, Talent, BadPersonality {
+    var singing: Level
+    var dancing: Level
+    var acting: Level
+    var frequencyOfCursing: Level
+    
+    init(name: String,
+         height: Int,
+         singing: Level,
+         dancing: Level,
+         acting: Level,
+         frequencyOfCursing: Level) {
+        self.singing = singing
+        self.dancing = dancing
+        self.acting = acting
+        self.frequencyOfCursing = frequencyOfCursing
+        super.init(name: name, height: height)
+    }
+}
