2017e45cc4c39fa160a0f5801203f1e89765a539
wolframhwang <hzw94@naver.com>
2022-05-11 23:57:49 +0900

    [feat] Mission5: Step1: AuditionManager에 관한 코드 구현
    - totalApplicant에 대해, 순회를 하면서 Mapping 시키고 추가하는 방식을 사용했고, 이에 대해 compactMap이 적당하다고 판단해서 사용했습니다.

--- CodeStarterCamp_Week5/AuditionManager.swift
+++ CodeStarterCamp_Week5/AuditionManager.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct AuditionManager {
+    let totalApplicantsList: [Person]
+    var passedApplicantsList: [Person] = []
+    
+    mutating func cast() {
+        passedApplicantsList += totalApplicantsList.compactMap { applicant in
+            if let applicantWithPersonality = (applicant as? TalentedPersonWithBadPersonality),
+               applicantWithPersonality.isPass() {
+                return applicant
+            }
+            
+            guard let applicantWitTalent = (applicant as? TalentedPerson), applicantWitTalent.isPass() else {
+                return nil
+            }
+            return applicant
+        }
+    }
+    
+    func printPassedApplicantsList() {
+        print("---합격자 명단---")
+        for applicant in passedApplicantsList {
+            print("\(applicant.name)")
+        }
+        print("""
+        --------------
+        축하합니다!!
+        """)
+    }
+}
