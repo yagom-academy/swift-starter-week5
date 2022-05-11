ff0acc17f0d9f48afd10408aead6ac50e376a4f1
wolframhwang <hzw94@naver.com>
2022-05-11 22:55:35 +0900

    [feat] Mission5: Step1: Person 클래스 구현

---
+++ CodeStarterCamp_Week5/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  CodeStarterCamp_Week5
+//
+//  Created by 황지웅 on 2022/05/11.
+//
+
+import Foundation
+
+class Person {
+    var name: String
+    var height: Int
+    
+    init(name: String, height: Int) {
+        self.name = name
+        self.height = height
+    }
+}
