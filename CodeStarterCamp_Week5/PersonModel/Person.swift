86cbca86d5b56a09bfdd5a5ecf03edae8692b4d1
jeongmin <lj845854@gmail.com>
2022-05-12 20:05:11 +0900

    style : 프로토콜 extension을 통해서 채택

---
+++ CodeStarterCamp_Week5/PersonModel/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  CodeStarterCamp_Week5
+//
+//  Created by 이정민 on 2022/05/10.
+//
+
+import Foundation
+
+class Person {
+    let name: String
+    let height: Int
+
+    init(name: String, height: Int) {
+        self.name = name
+        self.height = height
+    }
+}
+
