2d83ae92d1c924172bb485707cb1f2a26a5e5c5e
Jae Hyung Kim <smfc.mene@gmail.com>
2022-07-18 22:03:07 +0800

    Feat: Person class 생성

---
+++ CodeStarterCamp_Week5/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  CodeStarterCamp_Week5
+//
+//  Created by smfc on 18/7/2022.
+//
+
+import Foundation
+
+class Person {
+    let name: String
+    let height: Double
+    
+    func walk() {
+        print("\(self.name)이(가) 걷습니다.")
+    }
+}
