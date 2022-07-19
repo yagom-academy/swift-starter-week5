bdc66b09eea27dcdc9b31fbb22a1bf50da6c0bc0
Jae Hyung Kim <smfc.mene@gmail.com>
2022-07-19 14:14:21 +0800

    Feat: enum, protocol 선언

---
+++ CodeStarterCamp_Week5/Step1.swift
@@ -0,0 +1,30 @@
+//
+//  step1.swift
+//  CodeStarterCamp_Week5
+//
+//  Created by smfc on 18/7/2022.
+//
+
+import Foundation
+
+enum Level {
+    case A
+    case B
+    case C
+}
+
+protocol Talent {
+    var singing: Level { get set }
+    var dancing: Level { get set }
+    var acting: Level { get set }
+    
+    func sing()
+    func dance()
+    func act()
+}
+
+protocol BadPersonality {
+    var frequancyOfCursing: Level { get set }
+    
+    func curse()
+}
