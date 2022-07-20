2a01af67963b2996b89235c7454b5fbe9e56214f
Jae Hyung Kim <smfc.mene@gmail.com>
2022-07-20 22:33:04 +0800

    Feat: 지원자 인스턴스 생성 및 실행부 구현

--- CodeStarterCamp_Week5/main.swift
+++ CodeStarterCamp_Week5/main.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+let yagom = TalentedPerson(name: "yagom", height: 100, singing: .B, dancing: .A, acting: .C)
+let noroo = Person(name: "noroo", height: 1000)
+let summer = TalentedPerson(name: "summer", height: 900, singing: .B, dancing: .B, acting: .B)
+let coda = TalentedPerson(name: "coda", height: 200, singing: .A, dancing: .C, acting: .C)
+let odong = TalentedPersonWithBadPersonality(name: "odong", height: 400, singing: .A, dancing: .A, acting: .A, frequancyOfCursing: .A)
+
+var jae = AuditionManager(totalApplicantsList: [yagom, noroo, summer, coda, odong])
+jae.announcePassedApplicants()
