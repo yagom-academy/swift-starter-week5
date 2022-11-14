8077b22fdf7153df7e156300d2524da24db47567
500beckwon <kjsx60@gmail.com>
2022-11-14 19:46:39 +0900

    AuditionManager 구조체 제작

--- CodeStarterCamp_Week5/AuditionManager.swift
+++ CodeStarterCamp_Week5/AuditionManager.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct AuditionManager {
+    var totalApplicantsList: [Person]
+    var passedApplicantList: [Person] = []
+    
+    mutating func cast() {
+        guard !totalApplicantsList.isEmpty else { return }
+        let passedList = totalApplicantsList
+            .filter { $0 is Talent }
+            .filter { !($0 is BadPersonality) }
+            .filter { ($0 as? Talent)?.checkHighScoreLevel() == true }
+            
+        passedApplicantList.append(contentsOf: passedList)
+    }
+    
+    func announcePassedApplicants() {
+        guard !passedApplicantList.isEmpty else {
+            print("합격자가 없습니다")
+            return }
+        print("---합격자 명단---")
+        passedApplicantList
+            .forEach {
+                print($0.name)
+            }
+        print("""
+              --------------
+              축하합니다!!
+              """)
+    }
+}
