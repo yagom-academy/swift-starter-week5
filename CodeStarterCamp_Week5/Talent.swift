8fb21eaa1f15f5409738c417eb7dcd132d77e4ea
500beckwon <kjsx60@gmail.com>
2022-11-14 19:45:44 +0900

    Protocol 제작

--- CodeStarterCamp_Week5/Talent.swift
+++ CodeStarterCamp_Week5/Talent.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+protocol Talent {
+    var singing: Level { get set }
+    var dancing: Level { get set }
+    var acting: Level { get set }
+    func checkHighScoreLevel() -> Bool
+}
+
+extension Talent {
+    func checkHighScoreLevel() -> Bool {
+        return !([singing, dancing, acting]
+            .filter { $0 == .A }
+            .isEmpty)
+    }
+}
