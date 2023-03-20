6b584472e587363040eff54d6dc6752e9f9f448e
mint3382 <mskim0926@naver.com>
2023-03-20 16:14:30 +0900

    개행 수정

--- CodeStarterCamp_Week5/TalentedPersonWithBadPersonality.swift
+++ CodeStarterCamp_Week5/TalentedPersonWithBadPersonality.swift
@@ -9,11 +9,8 @@ import Foundation
 
 class TalentedPersonWithBadPersonality: Person, Talent, BadPersonality {
     var singing: Level
-    
     var dancing: Level
-    
     var acting: Level
-    
     var frequancyOfCursing: Level
     
     init(name: String, height: Int, singing: Level, dancing: Level, acting: Level, frequancyOfCursing: Level) {
