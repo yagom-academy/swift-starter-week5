f798d8c8b9d81cba07141d1d2fb6caff7fe09316
MaryJo-github <jotjdal0215@gmail.com>
2023-03-20 23:12:22 +0900

    feat: 합격자 추출 조건 추가 - auditionManager.swift: 합격자 추출조건 추가 및 그에 따른 합격자 리스트 업데이트

--- CodeStarterCamp_Week5/auditionManager.swift
+++ CodeStarterCamp_Week5/auditionManager.swift
@@ -13,16 +13,26 @@ struct AuditionManager {
     
     mutating func cast() {
         guard !totalApplicantsList.isEmpty else { print("지원자가 없습니다."); return }
-        // 하나라도 A등급 있는애들만 걸러
-        // frequancyOfCursing == 'A' 걸러
-        print(totalApplicantsList)
-        passedApplicantsList = totalApplicantsList
+
+        for applicant in totalApplicantsList {
+            if let person = applicant as? BadPersonality {
+                guard person.frequancyOfCursing != Level.A else { continue }
+            }
+            if let person = applicant as? TalentedPerson {
+                guard hasOutstandingTalent(person: person) else { continue }
+                passedApplicantsList.append(applicant)
+            }
+        }
     }
-    
+                
     func announcePassedApplicants() {
         guard !passedApplicantsList.isEmpty else { print("합격자가 없습니다."); return }
         print("---합격자 명단---")
         print(passedApplicantsList.map{ $0.name }.joined(separator: "\n"))
         print("--------------\n축하합니다!!")
+    }
+    
+    private func hasOutstandingTalent(person: TalentedPerson) -> Bool {
+        return person.singing == Level.A || person.dancing == Level.A || person.acting == Level.A
     }
 }
