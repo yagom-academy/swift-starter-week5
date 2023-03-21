6a618aafd3cb3eced1b4281bea84239f39820435
dduddupark <develop.dduddu@gmail.com>
2023-03-21 19:06:36 +0900

    step1 03/21 19:06

---
+++ CodeStarterCamp_Week5/BadPersonality.swift
@@ -0,0 +1,8 @@
+//
+//  BadPersonality.swift
+//  CodeStarterCamp_Week5
+//
+//  Created by suyeon park on 2023/03/21.
+//
+
+import Foundation
---
+++ CodeStarterCamp_Week5/TalentedPersonWithBadPersonality.swift
@@ -0,0 +1,8 @@
+//
+//  TalentedPersonWithBadPersonality.swift
+//  CodeStarterCamp_Week5
+//
+//  Created by suyeon park on 2023/03/21.
+//
+
+import Foundation
