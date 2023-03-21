c3099ab1ba6db917f952366b424deec1a0ae5f60
MaryJo-github <jotjdal0215@gmail.com>
2023-03-21 17:44:17 +0900

    refactor: 합격 조건을 manager의 메소드로 변경

--- CodeStarterCamp_Week5/auditionManager.swift
+++ CodeStarterCamp_Week5/auditionManager.swift
@@ -7,44 +7,42 @@
 
 import Foundation
 
-extension Talent {
-    var hasOutstandingTalent: Bool {
-        get {
-            self.singing == Level.A || self.dancing == Level.A || self.acting == Level.A
-        }
-    }
-}
-
-extension BadPersonality {
-    var isBadPerson: Bool {
-        get {
-            self.frequancyOfCursing == Level.A
-        }
-    }
-}
-
 struct AuditionManager {
     let totalApplicantsList: [Person]
     var passedApplicantsList: [Person] = []
     
     mutating func cast() {
-        guard !totalApplicantsList.isEmpty else { print("지원자가 없습니다."); return }
+        guard !totalApplicantsList.isEmpty else {
+            print("지원자가 없습니다.")
+            return
+        }
 
         for applicant in totalApplicantsList {
-            if let person = applicant as? BadPersonality {
-                guard !person.isBadPerson else { continue }
-            }
+            if let person = applicant as? BadPersonality, isBadPerson(person) { continue }
+
             if let person = applicant as? Talent {
-                guard person.hasOutstandingTalent else { continue }
+                guard hasOutstandingTalent(person) else { continue }
                 passedApplicantsList.append(applicant)
             }
         }
     }
                 
     func announcePassedApplicants() {
-        guard !passedApplicantsList.isEmpty else { print("합격자가 없습니다."); return }
+        guard !passedApplicantsList.isEmpty else {
+            print("합격자가 없습니다.")
+            return
+        }
+        
         print("---합격자 명단---")
         print(passedApplicantsList.map{ $0.name }.joined(separator: "\n"))
         print("--------------\n축하합니다!!")
+    }
+    
+    private func isBadPerson(_ person: BadPersonality) -> Bool {
+        return person.frequancyOfCursing == Level.A
+    }
+    
+    private func hasOutstandingTalent(_ person: Talent) -> Bool {
+        return person.singing == Level.A || person.dancing == Level.A || person.acting == Level.A
     }
 }
