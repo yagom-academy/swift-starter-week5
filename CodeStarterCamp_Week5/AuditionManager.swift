3d7d98daa1d950669ebea6e35d3f109b740e87f1
yagom <jin@whales-ui-MacBookAir.local>
2023-03-24 16:50:03 +0900

    Week5 [STEP1] Whales

--- CodeStarterCamp_Week5/AuditionManager.swift
+++ CodeStarterCamp_Week5/AuditionManager.swift
@@ -12,14 +12,13 @@ struct AuditionManager {
     var passedApplicantsList: [Person] = []
     
     mutating func cast(applicants: [Person]) {
-
         for applicant in applicants {
-            if let talentedApplicant = applicant as? Talent { // 재능있는 사람 선발
-                if talentedApplicant is TalentedPersonWithBadPersonality { // 인성거름
-                    return // 그냥 넘어감
+            if let talentedApplicant = applicant as? Talent {
+                if talentedApplicant is TalentedPersonWithBadPersonality {
+                    return
                 }
                 if talentedApplicant.singing == .A || talentedApplicant.dancing == .A ||  talentedApplicant.acting == .A {
-                    passedListUp(talentedApplicant) // A 면 합격
+                    passedListUp(talentedApplicant)
                 }
             }
         }
