ea191fbc14dc8fa8a50e6e96ae78a57967eee71c
yagom <jin@whales-ui-MacBookAir.local>
2023-03-24 16:48:30 +0900

    Week5 [STEP1] Whales

--- CodeStarterCamp_Week5/AuditionManager.swift
+++ CodeStarterCamp_Week5/AuditionManager.swift
@@ -14,22 +14,27 @@ struct AuditionManager {
     mutating func cast(applicants: [Person]) {
 
         for applicant in applicants {
-            if let applicant = applicant as? TalentedPerson {
-                if applicant.singing == .A {
-                    passedApplicantsList.append(applicant)
-                } else if applicant.dancing == .A {
-                    passedApplicantsList.append(applicant)
-                } else if applicant.acting == .A {
-                    passedApplicantsList.append(applicant)
+            if let talentedApplicant = applicant as? Talent { // 재능있는 사람 선발
+                if talentedApplicant is TalentedPersonWithBadPersonality { // 인성거름
+                    return // 그냥 넘어감
+                }
+                if talentedApplicant.singing == .A || talentedApplicant.dancing == .A ||  talentedApplicant.acting == .A {
+                    passedListUp(talentedApplicant) // A 면 합격
                 }
             }
         }
     }
     
+    mutating func passedListUp(_ applicant: Talent) {
+        if let passedApplicant = applicant as? Person {
+            passedApplicantsList.append(passedApplicant)
+        }
+    }
+    
     func announcePassedApplicants(passedList: [Person]) {
         print("---합격자 명단---")
-        for successApplicants in passedList {
-            print(successApplicants.name)
+        for passedApplicants in passedList {
+            print(passedApplicants.name)
         }
         print("--------------\n축하합니다!!")
     }
